6fa54d89243c3b07bcca2ba1c8aa26fbbffdb2bf
JuanCarlosCalvarioSilva <jcalvario@ucol.mx>
2017-04-23 13:54:43 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Control_de_inventario
+{
+    public partial class frmprincipal : Form
+    {
+        public frmprincipal()
+        {
+            InitializeComponent();
+        }
+
+        Inventario inventario = new Inventario();
+        Producto nuevoProducto; 
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            //Busqueda por código
+            Producto x = inventario.buscar(int.Parse(txtCodigo.Text));
+            if (x == null)
+            {
+                MessageBox.Show("No se encontro ningun registro con ese código");
+                txtNombre.Text = "";
+                txtCantidad.Text = "";
+                txtCosto.Text = "";
+            }                
+            else
+            {
+                txtCodigo.Text = x.codigo.ToString();
+                txtNombre.Text = x.nombre.ToString();
+                txtCantidad.Text = x.cantidad.ToString();
+                txtCosto.Text = x.costo.ToString();
+            }                                    
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            //Agrega producto
+            nuevoProducto = new Producto();
+
+            if (txtCodigo.Text == "" || txtNombre.Text ==""|| txtCantidad.Text ==""|| txtCosto.Text == "")
+                MessageBox.Show("Llenar todos los campos");
+            else
+            {
+                nuevoProducto.codigo = int.Parse(txtCodigo.Text);
+                nuevoProducto.nombre = txtNombre.Text;
+                nuevoProducto.cantidad = int.Parse(txtCantidad.Text);
+                nuevoProducto.costo = int.Parse(txtCosto.Text);
+
+                inventario.agregar(nuevoProducto);
+
+                txtCodigo.Text = "";
+                txtNombre.Text = "";
+                txtCantidad.Text = "";
+                txtCosto.Text = "";
+            }                          
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            //Elimina producto
+            inventario.borrar(int.Parse(txtCodigo.Text));
+            txtCodigo.Text = "";
+        }  
+
+        private void btnReporte_Click(object sender, EventArgs e)
+        {
+            //Reporte
+            txtReporte.Text = inventario.reporte();
+        }                
+    }
+}
---
+++ Inventario.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_de_inventario
+{
+    class Inventario
+    {        
+        Producto inicio, ultimo;         
+
+        public void agregar(Producto producto)
+        {
+            Producto anteriorPadre, actual;
+            actual = inicio;
+            anteriorPadre = null;
+
+            if (inicio == null)
+                inicio = producto;       
+            else if (ultimo.codigo < producto.codigo)
+            {
+                ultimo.siguiente = producto;                
+            }            
+            else
+            {                           
+                while (actual != null) 
+                {
+                    anteriorPadre = actual;
+                    actual = actual.siguiente;
+                    if (producto.codigo < actual.codigo)
+                    {
+                        anteriorPadre.siguiente = producto;
+                        producto.siguiente = actual;
+                        break;
+                    }                    
+                }                
+            }
+            ultimo = producto;
+        }
+
+        public Producto buscar(int codigo)
+        {    
+            Producto actual = inicio;
+            while (actual != null)
+            {
+                if (actual.codigo == codigo)
+                    return actual;
+                actual = actual.siguiente;
+            }
+            return null;
+        }
+
+        public void borrar(int codigo)
+        {
+            if (inicio != null)
+            {
+                Producto actual, anterior;
+                anterior = buscarAnteriorPadre(codigo);
+
+                if (anterior == null)
+                {
+                    actual = inicio;
+                    inicio = inicio.siguiente;
+                    anterior = inicio;
+                }
+                else
+                {
+                    actual = anterior.siguiente;
+                    anterior.siguiente = actual.siguiente;
+                }
+                actual = null;
+
+                if (anterior == null || anterior.siguiente == null)
+                    ultimo = anterior;
+            }        
+        }
+
+        private Producto buscarAnteriorPadre(int codigo)
+        {
+            Producto anteriorPadre, actual;
+            actual = inicio;
+            anteriorPadre = null;
+
+            while (actual != null)
+            {
+                if (actual.codigo == codigo)
+                    break;
+                anteriorPadre = actual;
+                actual = actual.siguiente;
+            }
+            return anteriorPadre;
+        }
+
+        public string reporte()
+        {
+            string cad = "";
+
+            if (inicio != null)
+            {
+                Producto actual = inicio;
+                while (actual != null)
+                {
+                    cad += actual.ToString()+ Environment.NewLine;
+                    actual = actual.siguiente;
+                }
+            }
+            return cad;
+        }            
+    }
+}
